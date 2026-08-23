b8a52ae4eac12c53f0e2201e32d0158976dce3c8
lrkr <git@lrkr.dev>
2026-08-22 21:17:48 -0400

    Fix nosimd stub signature

--- resample_nosimd.go
+++ resample_nosimd.go
@@ -4,4 +4,4 @@ package resample
 
 const Simd = false
 
-func resamplerProcessSimdF32(s *Resampler[float32], in ...[]float32) {}
+func resamplerProcessSimdF32(s *Resampler[float32], in [][]float32) {}
